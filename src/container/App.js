3d856fd4a79a5ece5e63694ae3e4730279bd5460
Axel <lezinaxel@gmail.com>
2017-02-26 16:41:05 +0100

    Props and State

--- src/container/App.js
+++ src/container/App.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      title : 'Hello World'
+    }
+  }
+  update(e){
+    console.log(e.target.name)
+    this.setState({
+      title : e.target.value
+    })
+  }
   render(){
-    return <h1>Hello World</h1>;
+    return (
+      <div>
+      <Hello/>
+        <input onChange={this.update.bind(this)} type="text" name="test"/>
+        <input onChange={this.update.bind(this)} type="text" name="coucou"/>
+      </div>
+    )
   }
 }
+
+
+class Hello extends Component {
+  render(){
+    return <h1>Hello World change</h1>
+  }
+}
+
+
+
+
+
+
+
+
+
+
 
 export default App;
